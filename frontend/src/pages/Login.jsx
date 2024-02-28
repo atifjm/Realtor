d0583eba80f8d3ff48dce7c25e0a4050b1e9b5f9
Atif Malik <atifjm@hotmail.com>
2024-02-28 20:35:56 +0500

    Completed Login and Registration pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import { useState } from "react";
+import "../styles/Login.scss";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  const [formData, setFormData] = useState({});
+  const navigate = useNavigate();
 
-export default Login
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/auth/login", formData);
+      if (res.data) {
+        alert(res.data.message)
+        navigate("/");
+      } else {
+        alert(res.message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="login">
+      <h2>Login</h2>
+      <form className="form-login" onSubmit={handleSubmit}>
+        <input
+          className="input-field"
+          type="text"
+          placeholder="Enter Username"
+          name="username"
+          onChange={handleChange}
+        />
+        {/*<input
+          className="input-field"
+          type="email"
+          placeholder="Enter Email"
+          name="email"
+          onChange={handleChange}
+        />
+        */}
+        <input
+          className="input-field"
+          type="password"
+          placeholder="Enter Password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button className="input-button" type="submit">
+          LOGIN
+        </button>
+      </form>
+      <div className="form-bottom">
+        <p className="para-line">Don't have an account ? </p>
+        <Link to="/register" className="link-login links">
+          Register
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import { useState } from "react";
+import "../styles/Register.scss";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
-  return (
-    <div>
-      Register
-    </div>
-  )
-}
+  const [formData, setFormData] = useState({});
+  const navigate = useNavigate();
 
-export default Register
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/auth/register", formData);
+      if (res.data) {
+        alert(res.data.message)
+        navigate("/login");
+      } else {
+        alert(res.message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="register">
+      <h2>Register</h2>
+      <form className="form-register" onSubmit={handleSubmit}>
+        <input
+          className="input-field"
+          type="text"
+          placeholder="Enter Username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          className="input-field"
+          type="email"
+          placeholder="Enter Email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          className="input-field"
+          type="password"
+          placeholder="Enter Password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button className="input-button" type="submit">
+          REGISTER
+        </button>
+      </form>
+      <div className="form-bottom">
+        <p className="para-line">Already have an account ? </p>
+        <Link to="/login" className="link-login links">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
