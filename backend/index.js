1d6db184fca6ab42fc54c7190d2d001f2c46f4d2
Atif Malik <atifjm@hotmail.com>
2024-02-28 15:24:27 +0500

    Connected to Mongo DB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
+const app = express();
+dotenv.config();
 
-const app = express()
+app.use(express.json());
+app.use(cors());
 
-app.listen(8080, ()=>{
-    console.log("Server is live on port 8080")
-})
+const connect = () => {
+  try {
+    mongoose.connect(process.env.MONGO_URL);
+    console.log("DB Connection is successful");
+  } catch (err) {
+    console.log("Error while connecting Mongo DB", err);
+  }
+};
+
+app.listen(8080, () => {
+  connect();
+  console.log("Server is live on port 8080");
+});
