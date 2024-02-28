84c8ce2810da9cf2cabca7f791f9c35f6ffc1310
Atif Malik <atifjm@hotmail.com>
2024-02-28 13:58:48 +0500

    Created pages and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css';
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
-  );
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
+    </Routes>
+    </BrowserRouter>
+    
+    );
 }
 
 export default App;
